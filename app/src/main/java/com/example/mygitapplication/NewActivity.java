4b5c297ca2381b0e1674d9973bfb742fa5deb1c5
Oliver911Lc <506089793@qq.com>
2021-01-23 20:18:33 +0800

    新增NewActivity

---
+++ app/src/main/java/com/example/mygitapplication/NewActivity.java
@@ -0,0 +1,13 @@
+package com.example.mygitapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class NewActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
