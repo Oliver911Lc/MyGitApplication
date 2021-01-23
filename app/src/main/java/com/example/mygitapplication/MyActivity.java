820e60227ed388b515bd1fea8ecb7c3290182763
LuChao911 <oliver_lc911@163.com>
2021-01-23 21:05:22 +0800

    我新加的类MyActivity

---
+++ app/src/main/java/com/example/mygitapplication/MyActivity.java
@@ -0,0 +1,14 @@
+package com.example.mygitapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MyActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //噼里啪啦一顿写
+    }
+}
